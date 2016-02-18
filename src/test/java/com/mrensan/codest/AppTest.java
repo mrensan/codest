7e1fc8af8ac5f23461bd8d2755ce8d97a7603530
shahram <shahram@gmail.com>
2016-02-19 00:16:10 +0330

    initial project

---
+++ src/test/java/com/mrensan/codest/AppTest.java
@@ -0,0 +1,17 @@
+package com.mrensan.codest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shahram on 2/18/16.
+ */
+public class AppTest {
+
+    @Test
+    public void harchemikhahaddelettangatbego() {
+        System.out.println("hello");
+        assertTrue(true);
+    }
+}
