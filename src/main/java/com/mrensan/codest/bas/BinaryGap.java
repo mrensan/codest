7a34aed84054a783be68e1efeb1f852faa373023
shahram <shahram@gmail.com>
2016-05-05 13:39:21 +0430

    Binary GAP calculator - Final improvements

--- src/main/java/com/mrensan/codest/bas/BinaryGap.java
+++ src/main/java/com/mrensan/codest/bas/BinaryGap.java
@@ -6,39 +6,42 @@ import java.util.List;
 
 /**
  * Calculates the BinaryGAP in a desired number.
+ * Lesson One in Codility Lessons
  *
  * @author shahram at gmail.com
  */
 public class BinaryGap {
 
-    public int calculateBinaryGap(int n) {
+    public int calculateBinaryGap(int N) {
 
-        StringBuilder bin = new StringBuilder();
-        List<Integer> binaryGap = new ArrayList<>();
+        int binaryGap = 0;
         int currentBG = 0;
         boolean canCount = false;
         int remain;
+        int oldRemain = 0;
 
         do {
-            remain = n % 2;
-            n = n / 2;
-            bin.insert(0, remain);
+            remain = N % 2;
+            N = N / 2;
 
+            if (!canCount && oldRemain == 1) {
+                canCount = true;
+            }
             if (remain == 1 && !canCount) {
                 canCount = true;
             } else if (remain == 1 && canCount) {
                 canCount = false;
-                binaryGap.add(currentBG);
+                if (currentBG > binaryGap) {
+                    binaryGap = currentBG;
+                }
                 currentBG = 0;
             } else if(canCount) {
                 currentBG++;
             }
+            oldRemain = remain;
 
-        } while (n > 0);
+        } while (N > 0);
 
-        if (binaryGap.size() == 0) {
-            return 0;
-        }
-        return Collections.max(binaryGap);
+        return binaryGap;
     }
 }
--- src/test/java/com/mrensan/codest/bas/BinaryGapTest.java
+++ src/test/java/com/mrensan/codest/bas/BinaryGapTest.java
@@ -12,13 +12,15 @@ public class BinaryGapTest {
     @Test
     public void calculateBinaryGapTest() {
         BinaryGap binaryGap = new BinaryGap();
-        int gap1 = binaryGap.calculateBinaryGap(45633);
-        int gap2 = binaryGap.calculateBinaryGap(15);
+        int gap1 = binaryGap.calculateBinaryGap(1610612737);
+        int gap5 = binaryGap.calculateBinaryGap(45634);
+        int gap2 = binaryGap.calculateBinaryGap(1041);
         int gap3 = binaryGap.calculateBinaryGap(16);
         int gap4 = binaryGap.calculateBinaryGap(29);
 
-        assertEquals("BinaryGap", 5, gap1);
-        assertEquals("BinaryGap", 0, gap2);
+        assertEquals("BinaryGap", 28, gap1);
+        assertEquals("BinaryGap", 4, gap5);
+        assertEquals("BinaryGap", 5, gap2);
         assertEquals("BinaryGap", 0, gap3);
         assertEquals("BinaryGap", 1, gap4);
     }
