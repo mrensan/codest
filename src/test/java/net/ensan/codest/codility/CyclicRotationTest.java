352b38a958728c7d4870c8b0fb6cdc97269aa318
Shahram Ensan <shahram.ensan@raisin.com>
2022-03-13 17:11:38 +0100

    cyclic rotation version 2

--- src/test/java/net/ensan/codest/codility/CyclicRotationTest.java
+++ src/test/java/net/ensan/codest/codility/CyclicRotationTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 
 public class CyclicRotationTest {
     @Test
-    public void calculateCyclicRotation() throws Exception {
+    public void calculateCyclicRotation() {
         CyclicRotation cyclicRotation = new CyclicRotation();
         assertArrayEquals(new int[] {}, cyclicRotation.calculateCyclicRotation(new int[] {}, 3));
         assertArrayEquals(new int[] {1}, cyclicRotation.calculateCyclicRotation(new int[] {1}, 3));
@@ -14,6 +14,18 @@ public class CyclicRotationTest {
         assertArrayEquals(new int[] {7,2,11,5,1}, cyclicRotation.calculateCyclicRotation(new int[] {5,1,7,2,11}, 3));
         assertArrayEquals(new int[] {5,1,7,2,11}, cyclicRotation.calculateCyclicRotation(new int[] {5,1,7,2,11}, 5));
         assertArrayEquals(new int[] {5,1,7,2,11}, cyclicRotation.calculateCyclicRotation(new int[] {5,1,7,2,11}, 50));
+
+    }
+
+    @Test
+    public void calculateCyclicRotation2() {
+        CyclicRotation cyclicRotation = new CyclicRotation();
+        assertArrayEquals(new int[] {}, cyclicRotation.calculateCyclicRotation2(new int[] {}, 3));
+        assertArrayEquals(new int[] {1}, cyclicRotation.calculateCyclicRotation2(new int[] {1}, 3));
+        assertArrayEquals(new int[] {9,1}, cyclicRotation.calculateCyclicRotation2(new int[] {1,9}, 3));
+        assertArrayEquals(new int[] {7,2,11,5,1}, cyclicRotation.calculateCyclicRotation2(new int[] {5,1,7,2,11}, 3));
+        assertArrayEquals(new int[] {5,1,7,2,11}, cyclicRotation.calculateCyclicRotation2(new int[] {5,1,7,2,11}, 5));
+        assertArrayEquals(new int[] {5,1,7,2,11}, cyclicRotation.calculateCyclicRotation2(new int[] {5,1,7,2,11}, 50));
 
     }
 
