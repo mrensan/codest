909b3cfb5763e5440a0b91c65243a48c39c410bf
mrensan <shahram@gmail.com>
2025-01-26 21:28:11 +0100

    rename to prefixSum

--- src/main/java/net/ensan/codest/codility/prefixsums/GenomicRangeQuery.java
+++ src/main/java/net/ensan/codest/codility/prefixsums/GenomicRangeQuery.java
@@ -6,25 +6,25 @@ public class GenomicRangeQuery {
     public int[] solution(String S, int[] P, int[] Q) {
         int[] result = new int[P.length];
         int len = S.length();
-        int[][] nextNucl = new int[4][len];
-        Arrays.fill(nextNucl[0], -1);
-        Arrays.fill(nextNucl[1], -1);
-        Arrays.fill(nextNucl[2], -1);
-        Arrays.fill(nextNucl[3], -1);
-        nextNucl[impactFactor(S.charAt(len - 1)) - 1][len - 1] =  len - 1;
+        int[][] prefixSum = new int[4][len];
+        Arrays.fill(prefixSum[0], -1);
+        Arrays.fill(prefixSum[1], -1);
+        Arrays.fill(prefixSum[2], -1);
+        Arrays.fill(prefixSum[3], -1);
+        prefixSum[impactFactor(S.charAt(len - 1)) - 1][len - 1] =  len - 1;
         for (int i = len - 2 ; i > -1 ; i--) {
-            nextNucl[0][i] = nextNucl[0][i + 1];
-            nextNucl[1][i] = nextNucl[1][i + 1];
-            nextNucl[2][i] = nextNucl[2][i + 1];
-            nextNucl[3][i] = nextNucl[3][i + 1];
-            nextNucl[impactFactor(S.charAt(i)) - 1][i] = i;
+            prefixSum[0][i] = prefixSum[0][i + 1];
+            prefixSum[1][i] = prefixSum[1][i + 1];
+            prefixSum[2][i] = prefixSum[2][i + 1];
+            prefixSum[3][i] = prefixSum[3][i + 1];
+            prefixSum[impactFactor(S.charAt(i)) - 1][i] = i;
         }
         for (int i = 0 ; i < P.length ; i++) {
-            if (nextNucl[0][P[i]] != -1 && nextNucl[0][P[i]] <= Q[i]) {
+            if (prefixSum[0][P[i]] != -1 && prefixSum[0][P[i]] <= Q[i]) {
                 result[i] = 1;
-            } else if (nextNucl[1][P[i]] != -1 && nextNucl[1][P[i]] <= Q[i]) {
+            } else if (prefixSum[1][P[i]] != -1 && prefixSum[1][P[i]] <= Q[i]) {
                 result[i] = 2;
-            } else if (nextNucl[2][P[i]] != -1 && nextNucl[2][P[i]] <= Q[i]) {
+            } else if (prefixSum[2][P[i]] != -1 && prefixSum[2][P[i]] <= Q[i]) {
                 result[i] = 3;
             } else {
                 result[i] = 4;
